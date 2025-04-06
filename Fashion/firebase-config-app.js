022d475b7dc50bf1a2c7791f2a809b5c590c3ad3
unknown <hemasunder20bathini@gmail.com>
2025-04-06 09:36:23 +0530

    Initial commit

---
+++ Fashion/firebase-config-app.js
@@ -0,0 +1,16 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyDH9YrBiBr3eakc30Kkvgjte0fPLTeNKp0",
+    authDomain: "store-148c8.firebaseapp.com",
+    databaseURL: "https://store-148c8-default-rtdb.firebaseio.com",
+    projectId: "store-148c8",
+    storageBucket: "store-148c8.appspot.com",
+    messagingSenderId: "227269307302",
+    appId: "1:227269307302:web:bcefae3cb039f697c9fd67"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const database = firebase.database();
+  
+  
